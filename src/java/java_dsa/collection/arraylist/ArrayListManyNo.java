3414a8d44fc66250ad0f9a158099510e5be312c0
Lalit <lalitgupta1qq@gmail.com>
2025-10-11 23:56:15 +0530

    ArrayList add the many element using for loop and implement many method

---
+++ src/java/java_dsa/collection/arraylist/ArrayListManyNo.java
@@ -0,0 +1,20 @@
+package java_dsa.collection.arraylist;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class ArrayListManyNo {
+    public static void main(String[] args) {
+        List<Integer> num = new ArrayList();
+        for (int i = 0; i< 100; i++){
+            num.add(i);
+        }
+        System.out.println(num);
+        System.out.println(num.contains(50));
+        System.out.println(num.stream().count());
+        System.out.println(num.isEmpty());
+        System.out.println(num.size());
+        System.out.println(num.lastIndexOf(99));
+    }
+}
