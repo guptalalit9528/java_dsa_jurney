1c53f2bd7f6c93ad9d4db3df6c5472c30f76ee3f
Lalit <lalitgupta1qq@gmail.com>
2025-10-11 23:41:10 +0530

    diffrence between egual or == to

---
+++ src/java/java_dsa/collection/arraylist/ArrayListEqualCheck.java
@@ -0,0 +1,22 @@
+package java_dsa.collection.arraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListEqualCheck {
+    public static void main(String[] args) {
+        List<Integer> list1 = new ArrayList<>();
+        list1.add(23);
+        List<Integer> list2 = new ArrayList<>();
+        list2.add(23);
+
+        System.out.println(list1.equals(list2));
+
+        if(list1 == list2){
+            System.out.println("True");
+        }
+        else{
+            System.out.println("False");
+        }
+    }
+}
